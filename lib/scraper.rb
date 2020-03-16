412aafe937659b3a903f730fe27480c1e9d96f19
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-16 06:21:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,6 @@ class Scraper
       card.css(".student-card a").each do |student|
         student_profile_link = "#{student.attr('href')}" #selectors found in inspector divs
         student_location = student.css('.student-location').text
-        puts student.css('.student-name').text
         student_name = student.css('.student-name').text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
